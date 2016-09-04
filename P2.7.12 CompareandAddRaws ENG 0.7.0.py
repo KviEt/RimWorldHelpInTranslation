eca5a35a369c12a668ffe1da1545599748341c60
KviEt <KviEt90@gmail.com>
2016-09-04 15:54:41 +0300

    Update
    Rule for writing Result.doc is changed.

--- P2.7.12 CompareandAddRaws ENG 0.7.0.py
+++ P2.7.12 CompareandAddRaws ENG 0.7.0.py
@@ -36,12 +36,10 @@ allTextExcess = {}
 allDefClass = {}
 XMLFiles = {}
 XMLNewFiles = {}
-
-i = 0
-countTags = len(tagText)
-while(i < countTags):
-    tagText[i] = tagText[i].lower()
-    i = i + 1
+outMissedResult = {}
+outPerhaps = []
+outWarning = []
+outAbsent = []
 
 def addElement(fileXML, elementText, text, nameDef, addTo):
     if("subsounds" not in elementText.lower()):
@@ -126,10 +124,9 @@ def findText(element, tagPath, li, fileXML, nameDef, stage, className, parentNam
                     result = expectSTR.match(text)
                 if(result and text != u"false" and text != u"true"):
                     if(tagPath):
-                        print u"\nPerhaps %s necessary to translate"%(tagPath + u"." + subElement.tag) + u", text:"
+                        outPerhaps.append(u"\n\nPerhaps %s necessary to translate"%(tagPath + u"." + subElement.tag) + u", text: %s\n"%text)
                     else:
-                        print u"\nPerhaps %s necessary to translate"%("None" + u"." + subElement.tag) + u", text:"
-                    print text
+                        outPerhaps.append(u"\n\nPerhaps %s necessary to translate"%("None" + u"." + subElement.tag) + u", text: %s\n"%text)
 
 def findDef(root, fileXML):
     for element in root:
@@ -237,17 +234,13 @@ def readXMLORG(funcSearch):
         if os.path.isdir(sub): 
             findXMLORG(sub, funcSearch)
 
-readXMLORG(findAbstract)
-
 def compareVar(addressParent, derivativeVars):
     for derivativeVar in derivativeVars:
         if(derivativeVar[0] == addressParent):
             return derivativeVar
     return None
 
-defineNumber = re.compile("[0-9]+")
-
-def addDerVars(derivativeVarsOrg, nameIndexEnd, numberDer, listOfVars, derivativeVars):
+def addDerVars(derivativeVarsOrg, nameIndexEnd, numberDer, listOfVars, derivativeVars, nameOfList, defineNumber):
     numberDerOrgExpect = 0
     for derivativeVarOrg in derivativeVarsOrg:
         if(nameOfList == derivativeVarOrg[0][:nameIndexEnd]):
@@ -264,164 +257,210 @@ def addDerVars(derivativeVarsOrg, nameIndexEnd, numberDer, listOfVars, derivativ
             derivativeVar = compareVar(derivativeVarOrg[0], derivativeVars)
             derivativeVars.remove(derivativeVar)
 
-for defName in allDefClass:
-    allDef = allDefClass[defName]
-    for className in allDef:
-        if(not allDef[className][0]):
-            relations = [className]
-            parent = allDef[className][1]
-            while(parent):
-                relations.append(parent)
-                if(parent in allDef):
-                    parent = allDef[parent][1]
-                else:
-                    parent = None
-            relations.reverse()
-            listOfVars = []
-            countOfParents = len(relations)
-            while(countOfParents != 1):
-                parent = relations[0]
-                derivative = relations[1]
-                derivativeVars = allDef[derivative][2][:]
-                nameOfList = None
-                nameIndexEnd = None
-                numberDer = None
-                if(listOfVars):
-                    parentList = listOfVars[:]
-                else:
+def writingOutFile(outFile, outList):
+   for strOut in outList:
+      outFile.write(strOut)
+
+def main():
+    
+    if(not os.path.exists(pathOriginal)):
+        print "Defs folder is necessary for execution of program."
+        return None
+
+    if(not os.path.exists(pathTranslation)):
+        print "Defs folder is necessary for execution of program."
+        return None
+
+    i = 0
+    countTags = len(tagText)
+    while(i < countTags):
+        tagText[i] = tagText[i].lower()
+        i = i + 1
+
+    readXMLORG(findAbstract)
+
+    defineNumber = re.compile("[0-9]+")
+
+    for defName in allDefClass:
+        allDef = allDefClass[defName]
+        for className in allDef:
+            if(not allDef[className][0]):
+                relations = [className]
+                parent = allDef[className][1]
+                while(parent):
+                    relations.append(parent)
                     if(parent in allDef):
-                        parentList = allDef[parent][2]
+                        parent = allDef[parent][1]
                     else:
-                        parentList = []
-                for parentVar in parentList:
-                    if(nameOfList and nameIndexEnd):
-                        if(nameOfList == parentVar[0][:nameIndexEnd]):
-                            numberMatch = defineNumber.search(parentVar[0])
-                            numberDer = numberMatch.group(0)
+                        parent = None
+                relations.reverse()
+                listOfVars = []
+                countOfParents = len(relations)
+                while(countOfParents != 1):
+                    parent = relations[0]
+                    derivative = relations[1]
+                    derivativeVars = allDef[derivative][2][:]
+                    nameOfList = None
+                    nameIndexEnd = None
+                    numberDer = None
+                    if(listOfVars):
+                        parentList = listOfVars[:]
+                    else:
+                        if(parent in allDef):
+                            parentList = allDef[parent][2]
+                        else:
+                            parentList = []
+                    for parentVar in parentList:
+                        if(nameOfList and nameIndexEnd):
+                            if(nameOfList == parentVar[0][:nameIndexEnd]):
+                                numberMatch = defineNumber.search(parentVar[0])
+                                numberDer = numberMatch.group(0)
+                                exists = compareVar(parentVar[0], listOfVars)
+                                if(not exists):
+                                    listOfVars.append(parentVar)
+                                continue
+                            else:
+                                addDerVars(allDef[derivative][2], nameIndexEnd, numberDer, listOfVars, derivativeVars, nameOfList, defineNumber)
+                                nameOfList = None
+                                nameIndexEnd = None
+                                numberDer = None
+                        derivativeVar = compareVar(parentVar[0], derivativeVars)
+                        if(derivativeVar):
+                            addressDerivative = derivativeVar[0]
+                            numberMatch = defineNumber.search(addressDerivative)
+                            if(numberMatch):
+                                numberDer = numberMatch.group(0)
+                                numberIndexEnd = numberMatch.end(0)
+                                nameIndexEnd = numberIndexEnd - len(numberDer)
+                                nameOfList = parentVar[0][:nameIndexEnd]
+                                listOfVars.append(parentVar)
+                                numberDer = int(numberDer) + 1
+                            else:
+                                listOfVars.append(derivativeVar)
+                                derivativeVars.remove(derivativeVar)
+                                if(parentVar in listOfVars):
+                                    listOfVars.remove(parentVar)
+                        else:
                             exists = compareVar(parentVar[0], listOfVars)
                             if(not exists):
                                 listOfVars.append(parentVar)
-                            continue
-                        else:
-                            addDerVars(allDef[derivative][2], nameIndexEnd, numberDer, listOfVars, derivativeVars)
-                            nameOfList = None
-                            nameIndexEnd = None
-                            numberDer = None
-                    derivativeVar = compareVar(parentVar[0], derivativeVars)
-                    if(derivativeVar):
-                        addressDerivative = derivativeVar[0]
-                        numberMatch = defineNumber.search(addressDerivative)
-                        if(numberMatch):
-                            numberDer = numberMatch.group(0)
-                            numberIndexEnd = numberMatch.end(0)
-                            nameIndexEnd = numberIndexEnd - len(numberDer)
-                            nameOfList = parentVar[0][:nameIndexEnd]
-                            listOfVars.append(parentVar)
-                            numberDer = int(numberDer) + 1
-                        else:
-                            listOfVars.append(derivativeVar)
-                            derivativeVars.remove(derivativeVar)
-                            if(parentVar in listOfVars):
-                                listOfVars.remove(parentVar)
-                    else:
-                        exists = compareVar(parentVar[0], listOfVars)
-                        if(not exists):
-                            listOfVars.append(parentVar)
-                if(nameOfList):
-                    addDerVars(allDef[derivative][2], nameIndexEnd, numberDer, listOfVars, derivativeVars)
-                for additionVar in derivativeVars:
-                    listOfVars.append(additionVar)
-                relations.pop(0)
-                countOfParents = len(relations)
-            if(defName not in allText):
-                allText[defName] = []
-            elementList = allText[defName]
-            fileXML = allDef[className][2][0][2]
-            for element in listOfVars:
-                if(element[1]):
-                    address = className + "." + element[0].lower()
-                    elementList.append(address) 
-                    allTextValue[address] = (element[1], fileXML)
+                    if(nameOfList):
+                        addDerVars(allDef[derivative][2], nameIndexEnd, numberDer, listOfVars, derivativeVars, nameOfList, defineNumber)
+                    for additionVar in derivativeVars:
+                        listOfVars.append(additionVar)
+                    relations.pop(0)
+                    countOfParents = len(relations)
+                if(defName not in allText):
+                    allText[defName] = []
+                elementList = allText[defName]
+                fileXML = allDef[className][2][0][2]
+                for element in listOfVars:
+                    if(element[1]):
+                        address = className + "." + element[0].lower()
+                        elementList.append(address) 
+                        allTextValue[address] = (element[1], fileXML)
 
-readXMLORG(findDef)
+    readXMLORG(findDef)
 
-listOfDir = os.listdir(pathTranslation)
-for nameDef in listOfDir:
-    notExpected = True
-    if(nameDef in allText):
-        notExpected = False
-        sub = os.path.join(pathTranslation, nameDef)
-        if os.path.isdir(sub): 
-            findXMLTranslation(sub, nameDef)
-    if((nameDef[-1] == 's' or nameDef[-1] == 'S') and nameDef[:-1] in allText):
-        notExpected = False
-        sub = os.path.join(pathTranslation, nameDef)
-        nameDef = nameDef[:-1]
-        if os.path.isdir(sub): 
-            findXMLTranslation(sub, nameDef)
-    if(notExpected):
-        print u"Warning! Not expected the %s folder in the DefInjected folder"%nameDef
+    listOfDir = os.listdir(pathTranslation)
+    for nameDef in listOfDir:
+        notExpected = True
+        if(nameDef in allText):
+            notExpected = False
+            sub = os.path.join(pathTranslation, nameDef)
+            if os.path.isdir(sub): 
+                findXMLTranslation(sub, nameDef)
+        if((nameDef[-1] == 's' or nameDef[-1] == 'S') and nameDef[:-1] in allText):
+            notExpected = False
+            sub = os.path.join(pathTranslation, nameDef)
+            nameDef = nameDef[:-1]
+            if os.path.isdir(sub): 
+                findXMLTranslation(sub, nameDef)
+        if(notExpected):
+            outWarning.append(u"\nWarning! Not expected %s folder in DefInjected folder\n"%nameDef)
 
-for key in allText.keys():
-    allElement = allText[key]
-    if(allElement):
+    for key in allText.keys():
+        allElement = allText[key]
+        if(allElement):
+            if(key not in outMissedResult):
+               outMissedResult[key] = {}
+            for element in allElement:
+                nameFile = allTextValue[element][1]
+                if(nameFile not in outMissedResult[key]):
+                   outMissedResult[key][nameFile] = []
+                path = os.path.join(pathTranslation, key)
+                if(not os.path.exists(path)):
+                    path = os.path.join(pathTranslation, key+'s')
+                XMLPath = os.path.join(path, nameFile)
+                if(os.path.isfile(XMLPath)):
+                    if(key not in XMLFiles):
+                        XMLFiles[key] = {}
+                    XMLFiles[key][nameFile] = os.path.join(path, nameFile)
+                if(key not in XMLNewFiles):
+                    XMLNewFiles[key] = {}
+                if(nameFile not in XMLNewFiles[key]):
+                    XMLNewFiles[key][nameFile] = []
+                XMLNewFiles[key][nameFile].append(element)
+                outMissedResult[key][nameFile].append(u"\n\n\n%s with text:\n\t%s\n"%(element, allTextValue[element][0]))
+
+    for key in allTextExcess.keys():
+        outAbsent.append(u"\n\n%s file have labels which absent in Defs folder:\n"%key)
+        allElement = allTextExcess[key]
         for element in allElement:
-            nameFile = allTextValue[element][1]
-            print u"\nIn the %s folder at the %s file, missed:\n"%(key, nameFile),
-            path = os.path.join(pathTranslation, key)
-            if(not os.path.exists(path)):
-                path = os.path.join(pathTranslation, key+'s')
-            XMLPath = os.path.join(path, nameFile)
-            if(os.path.isfile(XMLPath)):
-                if(key not in XMLFiles):
-                    XMLFiles[key] = {}
-                XMLFiles[key][nameFile] = os.path.join(path, nameFile)
-            if(key not in XMLNewFiles):
-                XMLNewFiles[key] = {}
-            if(nameFile not in XMLNewFiles[key]):
-                XMLNewFiles[key][nameFile] = []
-            XMLNewFiles[key][nameFile].append(element)
-            print u"%s with text: %s"%(element, allTextValue[element][0])
+            outAbsent.append(u"\n\t%s\n"%element)
 
-for key in allTextExcess.keys():
-    print u"\nThe %s file have labels which absent in the defs folder:"%key
-    allElement = allTextExcess[key]
-    for element in allElement:
-        print u"%s"%element
+    for defName in XMLNewFiles.keys():
+        for nameFile in XMLNewFiles[defName].keys():
+            if(defName in XMLFiles and nameFile in XMLFiles[defName]):
+                XMLPath = XMLFiles[defName][nameFile]
+                XMLParser = CParser(encoding='utf-8')
+                tree = ET.parse(XMLPath, XMLParser)
+                root = tree.getroot()
+            else:
+                tree = ET.ElementTree()
+                newRoot = ET.Element(u"LanguageData")
+                tree._setroot(newRoot)
+                root = tree.getroot()
+            for element in XMLNewFiles[defName][nameFile]:
+                subElement = ET.SubElement(root, element)
+                subElement.text = allTextValue[element][0]
+            XMLNewPath = os.path.join(pathTranslationOrg, defName)
+            if(not os.path.exists(XMLNewPath)):
+                os.makedirs(XMLNewPath)
+            XMLNewPath = os.path.join(XMLNewPath, nameFile)
+            xml = codecs.open(XMLNewPath, "w", "utf-8")
+            xml.write(u'\ufeff')
+            xml.write(u"<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n")
+            xml.write(u"<%s>\n"%root.tag)
+            for element in root:
+                tag = element.tag
+                if(tag is ET.Comment):
+                   xml.write(u"\n\t<!--")
+                else:
+                   xml.write(u"\n\t<%s>"%tag)
+                xml.write(element.text)
+                if(tag is ET.Comment):
+                   xml.write(u"-->\n")
+                else:
+                   xml.write(u"</%s>\n"%tag)
+            xml.write(u"\n</%s>\n"%root.tag)
+            xml.close()
 
-for defName in XMLNewFiles.keys():
-    for nameFile in XMLNewFiles[defName].keys():
-        if(defName in XMLFiles and nameFile in XMLFiles[defName]):
-            XMLPath = XMLFiles[defName][nameFile]
-            XMLParser = CParser(encoding='utf-8')
-            tree = ET.parse(XMLPath, XMLParser)
-            root = tree.getroot()
-        else:
-            tree = ET.ElementTree()
-            newRoot = ET.Element(u"LanguageData")
-            tree._setroot(newRoot)
-            root = tree.getroot()
-        for element in XMLNewFiles[defName][nameFile]:
-            subElement = ET.SubElement(root, element)
-            subElement.text = allTextValue[element][0]
-        XMLNewPath = os.path.join(pathTranslationOrg, defName)
-        if(not os.path.exists(XMLNewPath)):
-            os.makedirs(XMLNewPath)
-        XMLNewPath = os.path.join(XMLNewPath, nameFile)
-        xml = codecs.open(XMLNewPath, "w", "utf-8")
-        xml.write(u'\ufeff')
-        xml.write(u"<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n")
-        xml.write(u"<%s>\n"%root.tag)
-        for element in root:
-            tag = element.tag
-            if(tag is ET.Comment):
-               xml.write(u"\n\t<!--")
-            else:
-               xml.write(u"\n\t<%s>"%tag)
-            xml.write(element.text)
-            if(tag is ET.Comment):
-               xml.write(u"-->\n")
-            else:
-               xml.write(u"</%s>\n"%tag)
-        xml.write(u"\n</%s>\n"%root.tag)
-        xml.close()
+    outDoc = codecs.open("Result.doc", "w", "utf-8")
+    outDoc.write(u'\ufeff')
+    writingOutFile(outDoc, outPerhaps)
+    writingOutFile(outDoc, outWarning)
+    for defName in outMissedResult:
+       outDoc.write(u"\n\n=========================================================================")
+       outDoc.write(u"\nIn %s folder:"%defName)
+       for nameFile in outMissedResult[defName]:
+          outDoc.write(u"\n\n-------------------------------------------------------------------------")
+          outDoc.write(u"\nAt %s file:\n"%nameFile)
+          writingOutFile(outDoc, outMissedResult[defName][nameFile])
+          outDoc.write(u"\n-------------------------------------------------------------------------")
+       outDoc.write(u"\n=========================================================================")
+    writingOutFile(outDoc, outAbsent)
+    outDoc.close()
+
+if __name__ == "__main__":
+    main()
